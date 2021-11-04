f14ce7c8164127dad1848711fd8243cd241f1372
Hyperx3D <hyperx3d@gmail.com>
2021-11-04 13:04:03 +0100

    C:\Users\sklya\OneDrive\Desktop\SAP Kurs\git test 2\it-camp_gp_11-21

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(e => {
+	$("#starten").click(function(){
+		
+		
+		
+	
+	});
+});
+
+
+$(document).ready(e => {
+	$("#map").click(function(){
+		
+		
+		
+	
+	});
+});
+
+
+$(document).ready(e => {
+	$("#charakter").click(function(){
+		
+		
+		
+	
+	});
+});
